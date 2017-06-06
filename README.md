a88f063d48228022857540b6deae1b41d3e3b198
Viktor <vtet32@gmail.com>
2017-06-06 15:11:50 +0300

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# PeerplaysJS (peerplaysjs-lib)
+
+Pure JavaScript Peerplays/Graphene library for node.js and browsers. Can be used to construct, sign and broadcast transactions in JavaScript, and to easily obtain data from the blockchain via public apis.
+
+Most of this code was written by [jcalfeee](https://github.com/jcalfee).
---
+++ test/chain/ChainStore.js
@@ -0,0 +1,81 @@
+var assert = require("assert");
+var {Apis, ChainConfig} = require("peerplaysjs-ws");
+var { FetchChain, ChainStore } = require("../../lib/chain");
+
+var coreAsset;
+
+describe("ChainStore", () => {
+
+    // Connect once for all tests
+    before(function() {
+        return Apis.instance("wss://bitshares.openledger.info/ws", true).init_promise.then(function (result) {
+            coreAsset = result[0].network.core_asset;
+            ChainStore.init();
+        });
+    });
+
+    // Unsubscribe everything after each test
+    afterEach(function() {
+        ChainStore.subscribers = new Set();
+        ChainStore.clearCache();
+    });
+
+    after(function() {
+        ChainConfig.reset();
+    });
+
+    describe("Subscriptions", function() {
+
+        it("Asset not found", function() {
+            return new Promise( function(resolve) {
+                ChainStore.subscribe(function() {
+                    console.log("chainstore sub");
+                    assert(ChainStore.getAsset("NOTFOUND") === null)
+                    resolve()
+                })
+                assert(ChainStore.getAsset("NOTFOUND") === undefined)
+            })
+        })
+
+        it("Asset by name", function() {
+            return new Promise( function(resolve) {
+                ChainStore.subscribe(function() {
+                    assert(ChainStore.getAsset(coreAsset) != null)
+                    resolve()
+                })
+                assert(ChainStore.getAsset(coreAsset) === undefined)
+            })
+        })
+
+        it("Asset by id", function() {
+            return new Promise( function(resolve) {
+                ChainStore.subscribe(function() {
+                    assert(ChainStore.getAsset("1.3.0") != null)
+                    resolve()
+                })
+                assert(ChainStore.getAsset("1.3.0") === undefined)
+            })
+        })
+
+        it("Object by id", function() {
+            return new Promise( function(resolve) {
+                ChainStore.subscribe(function() {
+                    assert(ChainStore.getAsset("2.0.0") != null)
+                    resolve()
+                })
+                assert(ChainStore.getAsset("2.0.0") === undefined)
+            })
+        })
+
+
+    })
+        //     ChainStore.getAccount("not found")
+        //
+        //     ChainStore.unsubscribe(cb)
+        //     // return FetchChain("getAccount", "notfound")
+        //     let cb = res => console.log('res',res)
+        //     // })
+        // })
+
+
+})
