f1993582f33fb6f9e675de7da52c7ff53a2a8fb4
mseaward <m.seaward@pbsa.info>
2019-12-16 13:05:30 -0400

    chore: rebuild

--- dist/chain/src/ChainStore.js
+++ dist/chain/src/ChainStore.js
@@ -2273,4 +2273,4 @@ function FetchChain(methodName, objectIds) {
 chain_store.FetchChain = FetchChain;
 
 exports.default = chain_store;
-module.exports = exports['default'];
+module.exports = exports.default;
