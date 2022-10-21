cca9e0a6407e1cf2686986afb48b06774463533e
Evan Lynch <elynch@peerplays.com>
2022-10-21 14:22:08 -0400

    add transaction ids to get block

--- dist/serializer/src/operations.js
+++ dist/serializer/src/operations.js
@@ -581,7 +581,8 @@ var signed_block = new Serializer('signed_block', {
   transaction_merkle_root: bytes(20),
   extensions: set(future_extensions),
   witness_signature: bytes(65),
-  transactions: array(processed_transaction)
+  transactions: array(processed_transaction),
+  transaction_ids: array(bytes(65))
 });
 exports.signed_block = signed_block;
 var block_header = new Serializer('block_header', {
